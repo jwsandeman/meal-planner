b892c483a78f634c85e26305e042ac5458ab70ac
jwsandeman <jw.sandeman@gmail.com>
2022-09-30 07:46:11 +1000

    added project dependancies

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold underline">💛 Let's get the party started</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+};
