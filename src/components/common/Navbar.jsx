746d795176a488522f1c96812bf964d197bf4fef
jwsandeman <jw.sandeman@gmail.com>
2022-10-05 22:31:58 +1000

    added navbar styling to Navbar,jsx. added main ingredient select logic to FindRecipes.jsx

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,146 @@
 import React from 'react';
+import { useGlobalState } from '../../utils/stateContext.jsx';
 
-const navbar = () => {
-  return <div>navbar</div>;
+const Navbar = () => {
+  const { store } = useGlobalState();
+  const { loggedInUser } = store;
+
+  return (
+    <div className="navbar bg-neutral">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li tabIndex={0}>
+              <a className="justify-between">
+                Parent
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">Meal Planner</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <a>Find Recipes</a>
+          </li>
+          <li>
+            <a>Selected Recipes</a>
+          </li>
+          <li>
+            <a>Shopping List</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+          <div className="indicator">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+              />
+            </svg>
+            <span className="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+        </button>
+        {loggedInUser ? (
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <a className="btn bg-primary">Sign In</a>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default navbar;
+export default Navbar;
--- src/components/pages/FindRecipes.jsx
+++ src/components/pages/FindRecipes.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const FindRecipes = () => {
+  const mainIngredients = ['chicken', 'beef', 'lamb', 'pork', 'vegetarian', 'vegan'];
+
   return (
-    <div>
-      <h1 className="text-4xl font-bold underline">💛 Let's get the party started</h1>
-      <button className="btn">Button</button>
+    <div className="container mx-auto p-4 bg-green-500 artboard artboard-horizontal phone-6">
+      <div className="form-control">
+        <div className="input-group">
+          <select className="select select-bordered">
+            <option disabled selected>
+              Main Ingredient
+            </option>
+            {mainIngredients.map((ingredient) => {
+              return (
+                <option key={ingredient} value={ingredient}>
+                  {ingredient}
+                </option>
+              );
+            })}
+          </select>
+          <button className="btn">Go</button>
+        </div>
+      </div>
     </div>
   );
 };
