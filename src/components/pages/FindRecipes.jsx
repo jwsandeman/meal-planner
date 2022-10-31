9158cdb26a9ddaafb3e7c9956259b3a2465b8e67
jwsandeman <jw.sandeman@gmail.com>
2022-10-31 19:58:39 +1000

    added api env variables, added axios recipe call, added healType options.

--- src/components/pages/FindRecipes.jsx
+++ src/components/pages/FindRecipes.jsx
@@ -2,14 +2,59 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const FindRecipes = () => {
+  const { REACT_APP_EDAMAM_APP_ID, REACT_APP_EDAMAM_APP_KEY } = process.env;
+
   const initialFormData = {
     mainIngredient: '',
+    diet: '',
+    health: '',
+    cuisineType: '',
     mealType: '',
-    cuisineType: ''
+    dishType: ''
   };
   const mainIngredients = ['chicken', 'beef', 'lamb', 'pork', 'vegetarian', 'vegan'];
-  const mealTypes = ['breakfast', 'brunch', 'lunch/dinner', 'snack', 'teatime'];
+  const dietTypes = ['balanced', 'high-fiber', 'high-protein', 'low-carb', 'low-fat', 'low-sodium'];
+  const healthTypes = [
+    'alcohol-free',
+    'alcohol-cocktail',
+    'wheat-free',
+    'vegetarian',
+    'vegan',
+    'tree-nut-free',
+    'sulfite-free',
+    'sugar-conscious',
+    'soy-free',
+    'shellfish-free',
+    'sesame-free',
+    'red-meat-free',
+    'pork-free',
+    'pecatarian',
+    'peanut-free',
+    'paleo',
+    'No-oil-added',
+    'mustard-free',
+    'mollusk-free',
+    'Mediterranean',
+    'lupine-free',
+    'low-sugar',
+    'low-potassium',
+    'kosher',
+    'kidney-friendly',
+    'keto-friendly',
+    'immuno-supportive',
+    'gluten-free',
+    'fodmap-free',
+    'fish-free',
+    'egg-free',
+    'DASH',
+    'dairy-free',
+    'crustacean-free',
+    'celery-free'
+  ];
   const cuisineTypes = ['mexican', 'indian', 'bbq', 'american', 'aussie'];
+  const mealTypes = ['breakfast', 'brunch', 'lunch/dinner', 'snack', 'teatime'];
+  const dishTypes = [];
+
   const [formData, setFormData] = useState(initialFormData);
 
   const handleFormData = (e) => {
@@ -21,16 +66,30 @@ const FindRecipes = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // axios.get("",{
-    //   parameter: ["", ""],
-    //   authorization: ""
-    // })
-    // .then(response => {
-    //   console.log(response);
-    // })
-    // .catch(err => {
-    //   console.error(err);
-    // });
+    axios
+      .get(
+        `https://api.edamam.com/api/recipes/v2?type=public&app_id=${REACT_APP_EDAMAM_APP_ID}&app_key=${REACT_APP_EDAMAM_APP_KEY}`,
+        {
+          params: {
+            q: 'chicken',
+            diet: '',
+            health: 'keto-friendly',
+            cuisineType: '',
+            mealType: 'dinner',
+            DishType: ''
+          }
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    console.log(
+      '🚀 ~ file: FindRecipes.jsx ~ line 27 ~ handleSubmit ~ process.env.EDAMAM_APP_ID',
+      process.env.EDAMAM_APP_ID
+    );
   };
 
   useEffect(() => {
